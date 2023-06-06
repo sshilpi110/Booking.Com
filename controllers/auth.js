fceed109d1b3d240af3307eadbc6c67b783a8fd7
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-06-06 17:08:42 +0530

    user password hashed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const { UserModel } = require("../models/User")
-
+const bcrypt = require("bcrypt")
 const register = async (req, res, next) => {
     try {
+        const salt = bcrypt.genSaltSync(5);
+        const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new UserModel({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
         await newUser.save()
         res.status(200).send("User has been registered !")
