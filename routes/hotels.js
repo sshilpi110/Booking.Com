f71bcd7d032b913fbd0ba40a28bbeffaa16dfcff
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-06-06 14:38:05 +0530

    hotel updated part doneb

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,16 +1,25 @@
-const express=require("express")
+const express = require("express")
 const { HotelModel } = require("../models/Hotels")
-const hotelRoutes= express.Router()
-hotelRoutes.post("/",async(req,res)=>{
-    const newHotel=new HotelModel(req.body)
-    try{
-      const saveHotel= await newHotel.save()  
-      res.status(200).json(saveHotel)
-    }catch(err){
-       res.status(500).json(err)
+const hotelRoutes = express.Router()
+hotelRoutes.post("/", async (req, res) => {
+    const newHotel = new HotelModel(req.body)
+    try {
+        const saveHotel = await newHotel.save()
+        res.status(200).json(saveHotel)
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
-module.exports={
+hotelRoutes.put("/:id", async (req, res) => {
+    try {
+        const updateHotel = await HotelModel.findByIdAndUpdate(req.params.id, { $set: req.body },{new:true})
+        res.status(200).json(updateHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = {
     hotelRoutes
 }
